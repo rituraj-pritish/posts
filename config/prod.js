ed76237ed4e72721c3ba7b9e78450653bfa1abd8
rituraj-pritish <riturajpritish@gmail.com>
2020-02-11 23:05:20 +0530

    updated config file

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,9 @@
 module.exports = {
   mongoUri: process.env.MONGO_URI,
-  jwtKey: process.env.JWT_KEY
-}
+  jwtKey: process.env.JWT_KEY,
+  passwordForSocialSignUp: process.env.PASSWORD_FOR_SOCIAL_SIGN_UP,
+
+  cloudinaryName: process.env.CLOUDINARY_NAME,
+  cloudinaryKey: process.env.CLOUDINARY_KEY,
+  cloudinarySecret: process.env.CLOUDINARY_SECRET
+};
